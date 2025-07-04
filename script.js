53fc7c5c3055db4818c76d24e90604971f6028b5
itumeleng-itu <matomempaola@gmail.com>
2025-07-04 14:33:17 +0200

    updated

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const email = document.getElementById("email").value.trim();
 const updates = document.querySelector('input[name="updates"]:checked');
 
 form.addEventListener("submit", function (e) {
+    
+    const fullname = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const updates = document.querySelector('input[name="updates"]:checked');
 
     if (fullname === "" || email === "" || !updates) {
         alert("Please enter your details correctly.");
